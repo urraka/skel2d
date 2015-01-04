2b1d52498fdc7162ecd6136ab5dad978c2bd2370
urraka <perroazul64@gmail.com>
2015-01-04 11:28:18 -0300

    fixed error in flatten_quad (wrong point added) and added fill function, which will only work after calling stroke on convex closed shapes (and will probably screw up on inner bevel cases)

--- src/gfx_path.js
+++ src/gfx_path.js
@@ -41,15 +41,18 @@ function Path(device_pixel_ratio)
 	if (device_pixel_ratio === undefined)
 		device_pixel_ratio = 1;
 
-	this.line_width = 1;
+	this.stroke_width = 1;
 	this.line_cap = Path.Butt;
 	this.line_join = Path.Bevel;
-	this.miter_limit = 1;
-	this.color = [0, 0, 0, 255];
+	this.miter_limit = 10;
+	this.stroke_color = [0, 0, 0, 255];
+	this.fill_color = [0, 0, 0, 255];
 
 	this.commands = [];
 	this.points = [];
 	this.points_pool = [];
+	this.fill_list = [];
+	this.last_color = [0, 0, 0, 255];
 	this.closed = false;
 
 	this.device_pixel_ratio = device_pixel_ratio;
@@ -69,12 +72,18 @@ Path.prototype.begin = function(x, y)
 	var points = this.points;
 	var points_pool = this.points_pool;
 	var commands = this.commands;
+	var fill_list = this.fill_list;
 
 	while (points.length > 0)
 		points_pool.push(points.pop());
 
 	while (commands.length > 0)
 		commands.pop();
+
+	while (fill_list.length > 0)
+		fill_list.pop();
+
+	this.closed = false;
 
 	commands.push(MoveTo);
 	commands.push(x);
@@ -119,6 +128,45 @@ Path.prototype.close = function()
 	this.commands.push(Close);
 }
 
+Path.prototype.fill = function(vbo, ibo)
+{
+	var fill = this.fill_list;
+	var n = fill.length;
+
+	if (n > 0)
+	{
+		var same_color = true;
+		var last_color = this.last_color;
+		var fill_color = this.fill_color;
+
+		for (var i = 0; i < 4; i++)
+			same_color = (same_color && last_color[i] === fill_color[i]);
+
+		ibo.reserve(ibo.size + 3 * (n - 2));
+
+		if (same_color)
+		{
+			for (var i = 1; i < n - 1; i++)
+				ibo.push(fill[0], fill[i], fill[i + 1]);
+		}
+		else
+		{
+			vbo.reserve(vbo.size + n);
+
+			var base = vbo.size;
+
+			for (var i = 0; i < n; i++)
+			{
+				var vertex = vbo.get(fill[i]);
+				vbo.push(vertex.x, vertex.y, 0, 0, fill_color);
+			}
+
+			for (var i = 1; i < n - 1; i++)
+				ibo.push(base, base + i, base + i + 1);
+		}
+	}
+}
+
 Path.prototype.stroke = function(vbo, ibo)
 {
 	flatten(this);
@@ -129,10 +177,13 @@ Path.prototype.stroke = function(vbo, ibo)
 	var line_join = this.line_join;
 	var line_cap = this.line_cap;
 	var mlimit = this.miter_limit;
-	var rgba = this.color;
+	var rgba = this.stroke_color;
 	var loop = this.closed;
 
-	var w = this.line_width / 2;
+	for (var i = 0; i < 4; i++)
+		this.last_color[i] = rgba[i];
+
+	var w = this.stroke_width / 2;
 	var iw = w > 0 ? 1.0 / w : 0;
 
 	var ncap = Math.max(2, Math.ceil(Pi / (Math.acos(w / (w + this.tess_tol)) * 2.0)));
@@ -214,9 +265,10 @@ Path.prototype.stroke = function(vbo, ibo)
 	if (loop)
 	{
 		var start = vbo.size;
+		var fill_list = this.fill_list;
 
 		for (var i = npoints - 1, j = 0; j < npoints; i = j++)
-			add_join(vbo, ibo, points[i], points[j], w, ncap, line_join, rgba);
+			add_join(vbo, ibo, points[i], points[j], w, ncap, line_join, rgba, fill_list);
 
 		vbo.pushv(vbo.get(start + 0));
 		vbo.pushv(vbo.get(start + 1));
@@ -234,29 +286,44 @@ Path.prototype.stroke = function(vbo, ibo)
 	}
 }
 
-function alloc_point(path)
+function point_equals(x1, y1, x2, y2, tol)
 {
-	var pool = path.points_pool;
+	var dx = x2 - x1;
+	var dy = y2 - y1;
 
-	if (pool.length > 0)
-		return pool.pop();
-	else
-		return new Point();
+	return dx * dx + dy * dy < tol * tol;
+}
+
+function add_point(path, x, y, flags)
+{
+	var points = path.points;
+	var npoints = points.length;
+
+	if (npoints > 0)
+	{
+		var last = points[npoints - 1];
+
+		if (point_equals(last.x, last.y, x, y, path.dist_tol))
+		{
+			last.flags |= flags;
+			return;
+		}
+	}
+
+	var pool = path.points_pool;
+	var point = pool.length > 0 ? pool.pop() : new Point();
+
+	point.x = x;
+	point.y = y;
+	point.flags = flags;
+
+	points.push(point);
 }
 
 function flatten_bezier(path, x1, y1, x2, y2, x3, y3, x4, y4, level, type)
 {
 	if (level > 10)
 		return;
-
-	var x12  = 0.5 * (x1 + x2);
-	var y12  = 0.5 * (y1 + y2);
-	var x23  = 0.5 * (x2 + x3);
-	var y23  = 0.5 * (y2 + y3);
-	var x34  = 0.5 * (x3 + x4);
-	var y34  = 0.5 * (y3 + y4);
-	var x123 = 0.5 * (x12 + x23);
-	var y123 = 0.5 * (y12 + y23);
 
 	var dx = x4 - x1;
 	var dy = y4 - y1;
@@ -265,14 +332,18 @@ function flatten_bezier(path, x1, y1, x2, y2, x3, y3, x4, y4, level, type)
 
 	if ((d2 + d3) * (d2 + d3) < path.tess_tol * (dx * dx + dy * dy))
 	{
-		var p = alloc_point(path);
-		p.x = x4;
-		p.y = y4;
-		p.type = type;
-		path.points.push(p);
+		add_point(path, x4, y4, type);
 		return;
 	}
 
+	var x12   = 0.5 * (x1 + x2);
+	var y12   = 0.5 * (y1 + y2);
+	var x23   = 0.5 * (x2 + x3);
+	var y23   = 0.5 * (y2 + y3);
+	var x34   = 0.5 * (x3 + x4);
+	var y34   = 0.5 * (y3 + y4);
+	var x123  = 0.5 * (x12 + x23);
+	var y123  = 0.5 * (y12 + y23);
 	var x234  = 0.5 * (x23 + x34);
 	var y234  = 0.5 * (y23 + y34);
 	var x1234 = 0.5 * (x123 + x234);
@@ -287,26 +358,22 @@ function flatten_quad(path, x1, y1, x2, y2, x3, y3, level, type)
 	if (level > 10)
 		return;
 
+	var dx = x3 - x1;
+	var dy = y3 - y1;
+	var d = Math.abs(((x2 - x3) * dy - (y2 - y3) * dx));
+
+	if (d * d < path.tess_tol * (dx * dx + dy * dy))
+	{
+		add_point(path, x3, y3, type);
+		return;
+	}
+
 	var x12  = 0.5 * (x1 + x2);
 	var y12  = 0.5 * (y1 + y2);
 	var x23  = 0.5 * (x2 + x3);
 	var y23  = 0.5 * (y2 + y3);
 	var x123 = 0.5 * (x12 + x23);
 	var y123 = 0.5 * (y12 + y23);
-
-	var dx = x3 - x1;
-	var dy = y3 - y1;
-	var d = Math.abs(((x2 - x3) * dy - (y2 - y3) * dx));
-
-	if (d * d <= path.tess_tol * (dx * dx + dy * dy))
-	{
-		var p = alloc_point(path);
-		p.x = x123;
-		p.y = y123;
-		p.type = type;
-		path.points.push(p);
-		return;
-	}
 
 	flatten_quad(path, x1, y1, x12, y12, x123, y123, level + 1, 0);
 	flatten_quad(path, x123, y123, x23, y23, x3, y3, level + 1, type);
@@ -328,11 +395,10 @@ function flatten(path)
 			case MoveTo:
 			case LineTo:
 			{
-				var p = alloc_point(path);
-				p.x = commands[++i];
-				p.y = commands[++i];
-				p.type = PointCorner;
-				points.push(p);
+				var x = commands[++i];
+				var y = commands[++i];
+
+				add_point(path, x, y, PointCorner);
 			}
 			break;
 
@@ -370,6 +436,16 @@ function flatten(path)
 
 	var npoints = points.length;
 
+	var p0 = points[npoints - 1];
+	var p1 = points[0];
+
+	if (point_equals(p0.x, p0.y, p1.x, p1.y, path.dist_tol))
+	{
+		npoints--;
+		path.points_pool.push(points.pop());
+		path.closed = true;
+	}
+
 	for (var i = npoints - 1, j = 0; j < npoints; i = j++)
 	{
 		var p0 = points[i];
@@ -385,9 +461,10 @@ function flatten(path)
 	}
 }
 
-function add_join(vbo, ibo, p0, p1, w, ncap, line_join, rgba)
+function add_join(vbo, ibo, p0, p1, w, ncap, line_join, rgba, fill)
 {
 	var index = vbo.size;
+	var is_left = ((p1.flags & PointLeft) !== 0);
 
 	if ((p1.flags & (PointBevel | PointInnerBevel)) !== 0)
 	{
@@ -396,7 +473,6 @@ function add_join(vbo, ibo, p0, p1, w, ncap, line_join, rgba)
 		var dlx1 =  p1.dy;
 		var dly1 = -p1.dx;
 
-		var is_left = ((p1.flags & PointLeft) !== 0);
 		var bw = is_left ? w : -w;
 
 		var x0, y0, x1, y1;
@@ -449,6 +525,8 @@ function add_join(vbo, ibo, p0, p1, w, ncap, line_join, rgba)
 
 				ibo.push(center, center - 1, center + 2);
 				ibo.push(center, center + 1, center + 2);
+
+				if (fill) fill.push(index);
 			}
 			else
 			{
@@ -483,6 +561,8 @@ function add_join(vbo, ibo, p0, p1, w, ncap, line_join, rgba)
 				vbo.push(x1, y1, 0, 0, rgba);
 
 				ibo.push(center, center + (n - 1), center + n);
+
+				if (fill) fill.push(index + 1);
 			}
 		}
 		else
@@ -506,6 +586,8 @@ function add_join(vbo, ibo, p0, p1, w, ncap, line_join, rgba)
 
 				vbo.push(x1, y1, 0, 0, rgba);
 				vbo.push(p1.x - dlx1 * w, p1.y - dly1 * w, 0, 0, rgba);
+
+				if (fill) fill.push(index);
 			}
 			else
 			{
@@ -526,6 +608,8 @@ function add_join(vbo, ibo, p0, p1, w, ncap, line_join, rgba)
 
 				vbo.push(p1.x + dlx1 * w, p1.y + dly1 * w, 0, 0, rgba);
 				vbo.push(x1, y1, 0, 0, rgba);
+
+				if (fill) fill.push(index + 1);
 			}
 		}
 	}
@@ -533,6 +617,8 @@ function add_join(vbo, ibo, p0, p1, w, ncap, line_join, rgba)
 	{
 		vbo.push(p1.x + (p1.dmx * w), p1.y + (p1.dmy * w), 0, 0, rgba);
 		vbo.push(p1.x - (p1.dmx * w), p1.y - (p1.dmy * w), 0, 0, rgba);
+
+		if (fill) fill.push(is_left ? index : index + 1);
 	}
 
 	index = vbo.size;
